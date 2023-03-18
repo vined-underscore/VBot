00439f2bcbcde3cbc2f23d0d4b7d478044445fc7
vined <115558899+vined-underscore@users.noreply.github.com>
2023-03-18 23:18:22 +0100

    Update install.py

--- install.py
+++ install.py
@@ -5,3 +5,4 @@ os.system("python -m pip uninstall discord.py")
 os.system("python -m pip uninstall discord.py-self")
 
 os.system("python -m pip install git+https://github.com/dolfies/discord.py-self@renamed#egg=selfcord.py[voice]")
+os.system("python -m pip install -r requirements.txt")
