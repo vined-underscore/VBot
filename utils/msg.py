8ae1828a4bacdacd8b364887cd0907fe1eb2cbc3
vined_underscore <vinedquotation@gmail.com>
2023-03-18 23:10:43 +0100

    Added try statements

--- utils/msg.py
+++ utils/msg.py
@@ -1,13 +1,19 @@
 import asyncio
 import selfcord as discord
-from selfcord.ext import commands as vbot
+from selfcord.ext import commands
 
 delay = 5
 
 async def delete_msg(msg: discord.Message): # For some reason using
-    await asyncio.sleep(delay) # await msg.edit(delete_after = 5)
-    await msg.delete() # Makes a stupid loop
+    try:
+        await asyncio.sleep(delay) # await msg.edit(delete_after = 5)
+        await msg.delete() # Makes a stupid loop
+    except:
+        pass
 
-async def send_msg(msg: discord.Message, ctx: vbot.Context):
-    await ctx.send(msg)
-    await delete_msg(ctx.message, delay)
+async def send_msg(msg: discord.Message, ctx: commands.Context):
+    try:
+        await ctx.send(msg)
+        await delete_msg(ctx.message, delay)
+    except:
+        pass
