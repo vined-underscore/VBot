17b686646b51842d3801469d881b50b4c5948106
vined_underscore <vinedquotation@gmail.com>
2023-04-02 17:40:49 +0200

    Format + log()

--- utils/other.py
+++ utils/other.py
@@ -1,4 +1,14 @@
 import os
+from datetime import datetime
+from colorama import Fore as F
+from typing import Any
 
-def clear_console():
-  os.system("clear" if os.name != "nt" else "cls")
+
+def clear_console() -> None:
+    os.system("clear" if os.name != "nt" else "cls")
+
+
+def log(text: Any) -> None:
+    now = datetime.now()
+    time = now.strftime("%H:%M:%S %d-%m-%Y")
+    print(f"{F.LIGHTBLACK_EX}[{time}] {text}")
