29768c071a6a985cddc022d7836a1d4a3d03e65f
vined_underscore <vinedquotation@gmail.com>
2023-04-22 18:44:03 +0200

    Added extensive logging config

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
 class config:
-    global token; token = '' # Your token
+    global token; token = "" # Your account token
     global prefix; prefix = [
-        'v.',
-        '..',
-        '::'
+        "v.",
+        ",",
+        ">"
     ] # List of prefixes. Can be one or multiple
-    global logging; logging = True # Whether to log stuff like people leaving, joining etc.
+    
+    global logging; logging = {
+        "is_logging": True, # Logs stuff like server joins, leaves, commands
+        "error_logging": "both", # Error logging. Can be either "console" or "channel" or "both" or "" if you want there to be no error logging (bad idea)
+        "channel": {
+            "webhook_url": ""
+        } # Put blank to not log in a channel
+    }
 
-    global nitro_sniper; nitro_sniper = True
-    global nitro_sniper_url; nitro_sniper_url = '' # Nitro sniper URL
+    global nitro_sniper; nitro_sniper = {
+        "url": "", # Nitro sniper URL
+        "snipe": True,
+        "ping": True # If to ping @everyone when a code has been sniped
+    }
+    
