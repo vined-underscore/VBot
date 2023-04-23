5cb35227ddef3d0dc2e0074fd9ddfc5afd67e8f6
vined_underscore <vinedquotation@gmail.com>
2023-04-23 14:08:03 +0200

    Added snipers

--- config.py
+++ config.py
@@ -9,14 +9,32 @@ class config:
     global logging; logging = {
         "is_logging": True, # Logs stuff like server joins, leaves, commands
         "error_logging": "both", # Error logging. Can be either "console" or "channel" or "both" or "" if you want there to be no error logging (bad idea)
-        "channel": {
-            "webhook_url": ""
-        } # Put blank to not log in a channel
+        "url": ""
+        # Disable webhooks by changing it to ""
     }
 
-    global nitro_sniper; nitro_sniper = {
-        "url": "", # Nitro sniper URL
-        "snipe": True,
-        "ping": True # If to ping @everyone when a code has been sniped
+    # You can disable any of the webhooks by changing the URL to ""
+    global snipers; snipers = {
+        "nitro_sniper": {
+            "enabled": False,
+            "url": "", # Nitro sniper URL
+            "ping": True # If to ping @everyone when a code has been sniped
+        },
+        "invite_sniper": {
+            "enabled": True,
+            "url": "", # Invite sniper URL
+            "ping": False # If to ping @everyone when an invite has been found
+        },
+        "keyword_sniper": {
+            "enabled": True, # If to log keywords
+            "url": "", # Keyword sniper URL
+            "ping": True, # If to ping @everyone when a keyword has been found
+            
+            "keywords": [
+                "keyword 1",
+                "keyword 2",
+                "keyword 3"
+            ], # List of keywords to look out for
+        }
     }
     
