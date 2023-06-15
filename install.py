ec6a8a60db001cc95c9275db3f9e9d2343bddd71
vined <115558899+vined-underscore@users.noreply.github.com>
2023-06-15 18:13:10 +0200

    Fixed startup not working

--- install.py
+++ install.py
@@ -5,5 +5,5 @@ os.system("python -m pip uninstall discord.py")
 os.system("python -m pip uninstall discord.py-self")
 
 os.system("python -m pip uninstall selfcord.py")
-os.system("python -m pip install discord.py-self")
+os.system("python -m pip install git+https://github.com/dolfies/discord.py-self@085884923bf16fafe268ceb5bdf35e7aa0fe60e4#egg=selfcord.py[voice]")
 os.system("python -m pip install -r requirements.txt")
